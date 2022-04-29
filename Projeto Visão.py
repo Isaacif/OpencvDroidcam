4f485f039e70591593b3219ec0bd916065176c7e
Isaac Sousa <isaaclimadesousa08@gmail.com>
2022-04-29 09:32:23 -0300

    Change to QRcode reader

--- Projeto Visão.py
+++ Projeto Visão.py
@@ -1,5 +1,6 @@
 import cv2
 BLUE_COLOR = (255, 0, 0)
+import webbrowser
 
 STROKE = 2
 
@@ -9,21 +10,32 @@ clf = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml') #pode ser alterad
 stream = cv2.VideoCapture('http://192.168******/video.jpg') #endereço de ip pessoal da rede (Obtido pelo aplicativo DroidCam)
 
 while(not cv2.waitKey(20) & 0xFF == ord('q')):
+  
 
-        ret, frame = stream.read()
+       # ret, frame = stream.read()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+       # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        faces = clf.detectMultiScale(gray)
+       # faces = clf.detectMultiScale(gray)
 
-        for x, y, w, h in faces:
+       # for x, y, w, h in faces:
 
-            cv2.rectangle(frame, (x, y), (x+w, y+h), BLUE_COLOR, STROKE)
+        #    cv2.rectangle(frame, (x, y), (x+w, y+h), BLUE_COLOR, STROKE)
+        
+       # img = cap.read()
+        
+        img = cap.read()
+        data, box, _ = detector.detectorAndDecode(img)
+        
+        if data:
+                save = data
+                break
+                
+        cv2.imshow('IP Camera stream', img)
 
-        cv2.imshow('IP Camera stream',frame)
+        
+browser=webbrowser.open(str(save))
 
-
-
-stream.release()
+cap.release()
 
 cv2.destroyAllWindows()
